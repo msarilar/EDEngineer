e0119adc1cdef8b09d333cec797c89017a13cd55
git <maxime.sarilar@gmail.com>
2016-11-08 14:00:33 +0100

    prepare 1.0.0.23 version

--- EDEngineer/Properties/Settings.Designer.cs
+++ EDEngineer/Properties/Settings.Designer.cs
@@ -94,7 +94,7 @@ namespace EDEngineer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.0.0.22")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.0.0.23")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
