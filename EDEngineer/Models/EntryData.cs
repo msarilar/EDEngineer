fd29fdbb95015fbfb6589f2b0b3d9d43ba4b6ca0
git <maxime.sarilar@gmail.com>
2016-11-09 20:09:02 +0100

    made the json more human readable in case other developpers want to use them for their own apps

--- EDEngineer/Models/EntryData.cs
+++ EDEngineer/Models/EntryData.cs
@@ -1,10 +1,18 @@
-﻿namespace EDEngineer.Models
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace EDEngineer.Models
 {
     public class EntryData
     {
         public string Name { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
         public Rarity Rarity { get; set; }
+
         public string FormattedName { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
         public Kind Kind { get; set; }
     }
 }
--- EDEngineer/Properties/Settings.Designer.cs
+++ EDEngineer/Properties/Settings.Designer.cs
@@ -190,7 +190,7 @@ namespace EDEngineer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool ResetUI {
             get {
                 return ((bool)(this["ResetUI"]));
