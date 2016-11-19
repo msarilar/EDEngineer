c7e79681a50ca8ae476acf77828fe0b8250ff236
git <maxime.sarilar@gmail.com>
2016-11-20 00:22:24 +0100

    dynamic icons regarding origins

---
+++ EDEngineer/Converters/OriginToControlTemplateConverter.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using EDEngineer.Models;
+
+namespace EDEngineer.Converters
+{
+    public class OriginToControlTemplateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var origin = (Origin)value;
+            switch (origin)
+            {
+                case Origin.Mission:
+                    return Application.Current.FindResource("OriginMission");
+                case Origin.Mining:
+                    return Application.Current.FindResource("OriginMining");
+                case Origin.Scan:
+                    return Application.Current.FindResource("OriginScan");
+                case Origin.Salvage:
+                    return Application.Current.FindResource("OriginSalvage");
+                case Origin.Surface:
+                    return Application.Current.FindResource("OriginSurface");
+                case Origin.Market:
+                    return Application.Current.FindResource("OriginMarket");
+                case Origin.Unknown:
+                    return Application.Current.FindResource("OriginMission");
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
