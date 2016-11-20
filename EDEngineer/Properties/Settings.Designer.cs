38e7f13ed301de5739320394571a9ef127d20ce5
git <maxime.sarilar@gmail.com>
2016-11-20 12:21:13 +0100

    1.0.2 release notes

--- EDEngineer/Properties/Settings.Designer.cs
+++ EDEngineer/Properties/Settings.Designer.cs
@@ -94,7 +94,7 @@ namespace EDEngineer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.0.1.3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.0.2.0")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
