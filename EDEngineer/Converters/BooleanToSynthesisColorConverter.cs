abaf5a8087eee7832626b843df1a6187a38b4a99
git <maxime.sarilar@gmail.com>
2016-11-21 21:39:02 +0100

    fixed color of favorite button when selected

---
+++ EDEngineer/Converters/BooleanToSynthesisColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace EDEngineer.Converters
+{
+    public class BooleanToSynthesisColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var synthesis = (bool)value;
+            return synthesis ? SynthesisColor : NormalColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SolidColorBrush SynthesisColor { get; set; }
+        public SolidColorBrush NormalColor { get; set; }
+    }
+}
