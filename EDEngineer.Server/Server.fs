63b73a0802d12374ee25e684ae6acb71864030d2
git <maxime.sarilar@gmail.com>
2016-11-25 13:02:33 +0100

    csv export

--- EDEngineer.Server/Server.fs
+++ EDEngineer.Server/Server.fs
@@ -3,6 +3,8 @@
 open System
 open System.Collections.Generic
 open System.Linq
+open System.Data
+open System.IO
 
 open Suave
 open Suave.Filters
@@ -12,11 +14,13 @@ open Suave.RequestErrors
 open Newtonsoft.Json
 open NodaTime
 open NodaTime.Text
+open CsvHelper
 
 open EDEngineer.Models.Barda.Collections
 open EDEngineer.Models
 
 type Format = Json | Xml | Csv
+type cargoType = { Name: string; Count: int }
 
 type Cmdr<'TGood, 'TBad> = 
   | Found         of 'TGood
@@ -41,35 +45,60 @@ type CmdrBuilder() =
 
 let cmdr = CmdrBuilder()
 
+let dataTableToCsv = fun(t:DataTable) ->
+  use sWriter = new StringWriter()
+  use cWriter = new CsvWriter(sWriter)
+  let columns = t.Columns.Cast<DataColumn>() |> List.ofSeq
+  let rows = t.Rows.Cast<DataRow>() |> List.ofSeq
+
+  cWriter.Configuration.Delimiter <- ";"
+
+  columns
+    |> List.map (fun c -> cWriter.WriteField(c.ColumnName))
+    |> ignore
+
+  cWriter.NextRecord()
+
+  rows
+    |> List.map (fun r -> 
+      columns
+        |> List.map(fun c -> 
+          let content = r.[c.ColumnName].ToString()
+          cWriter.WriteField(content))
+        |> ignore
+      cWriter.NextRecord()
+    )
+    |> ignore
+
+  sWriter.ToString()
+
 let start (token, port, state:Func<IDictionary<string, State>>) =
 
   let json = fun s -> JsonConvert.SerializeObject s
   let xml = fun s -> 
-    let xmlDoc = (json(s) |> sprintf "{ \"ingredient\": %s }", "root") |> JsonConvert.DeserializeXmlNode
+    let xmlDoc = (json(s) |> sprintf "{ \"item\": %s }", "root") |> JsonConvert.DeserializeXmlNode
     xmlDoc.InnerXml
-  let csv = fun s -> json s
+  let csv = fun s -> JsonConvert.DeserializeObject<DataTable>(json(s)) |> dataTableToCsv
     
   let ingredients = fun (state:State) -> state.Cargo
-                                          |> List.ofSeq
-                                          |> List.map (fun d -> d.Value.Data)
+                                         |> List.ofSeq
+                                         |> List.map (fun d -> d.Value.Data)
 
   let blueprints = fun (state:State) -> state.Blueprints
                                         |> List.ofSeq
 
   let referenceData = fun (state:Func<IDictionary<string, State>>) -> state.Invoke().First().Value
 
-
   let cargoExtractor = fun (state:State, kind) -> state.Cargo
                                                   |> List.ofSeq
+                                                  |> List.map (fun e -> e.Value)
                                                   |> List.filter 
                                                     (fun e -> match kind with
-                                                              | Some(Kind.Commodity) -> e.Value.Data.Kind = Kind.Commodity
-                                                              | Some(Kind.Data)      -> e.Value.Data.Kind = Kind.Data
-                                                              | Some(Kind.Material)  -> e.Value.Data.Kind = Kind.Material
+                                                              | Some(Kind.Commodity) -> e.Data.Kind = Kind.Commodity
+                                                              | Some(Kind.Data)      -> e.Data.Kind = Kind.Data
+                                                              | Some(Kind.Material)  -> e.Data.Kind = Kind.Material
                                                               | _                    -> true)
-                                                  |> List.map (fun e -> e.Value)
-                                                  |> List.map (fun e -> e.Data.Name, e.Count)
-                                                  |> dict
+                                                  |> List.map (fun e -> { Name = e.Data.Name; Count = e.Count })
 
   let commanderRoute = fun commander ->
                          match state.Invoke().TryGetValue(commander) with
@@ -77,10 +106,10 @@ let start (token, port, state:Func<IDictionary<string, State>>) =
                          | (false, _)    -> NotFound commander
 
   let timeRoute = fun s -> match s with
-                            | Some(t) -> match InstantPattern.GeneralPattern.Parse(t) with
-                                         | e when e.Success = true -> Parsed e.Value
-                                         | _                       -> BadString t
-                            | None    -> Parsed Instant.MinValue
+                           | Some(t) -> match InstantPattern.GeneralPattern.Parse(t) with
+                                        | e when e.Success = true -> Parsed e.Value
+                                        | _                       -> BadString t
+                           | None    -> Parsed Instant.MinValue
 
   let AcceptExtractor = fun(request:HttpRequest) ->
     let accept = request.headers.Where(fun (k, v) -> k = "accept").Select(fun (k, v) -> v).First()
