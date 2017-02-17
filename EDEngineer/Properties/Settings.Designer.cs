f463b0d41642fcf32ac948b1602105f2e6f01f5a
git <maxime.sarilar@gmail.com>
2017-02-17 23:18:08 +0100

    prepare version 1.0.5.0

--- EDEngineer/Properties/Settings.Designer.cs
+++ EDEngineer/Properties/Settings.Designer.cs
@@ -94,7 +94,7 @@ namespace EDEngineer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.0.4.9")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.0.5.0")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
