04269961fb976c721560a730e36332f9c6655ccf
git <maxime.sarilar@gmail.com>
2017-10-29 10:02:11 +0100

    added Subkind enum

---
+++ EDEngineer.Models/Subkind.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel;
+
+namespace EDEngineer.Models
+{
+    public enum Subkind
+    {
+        [Description("Raw")]
+        Raw,
+
+        [Description("Manufactured")]
+        Manufactured
+    }
+}
