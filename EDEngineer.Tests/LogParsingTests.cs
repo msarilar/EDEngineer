698f23e08381202fb1af211304345f9b985c0827
git <maxime.sarilar@gmail.com>
2018-04-08 21:32:27 +0200

    unit test project

---
+++ EDEngineer.Tests/LogParsingTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Moq;
+using NFluent;
+using NUnit.Framework;
+
+namespace EDEngineer.Tests
+{
+    [TestFixture]
+    public class LogParsingTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            var flag = false;
+
+            // mocking interfaces:
+            var disposable = new Mock<IDisposable>();
+            disposable.Setup(d => d.Dispose()).Callback(() => flag = true);
+
+            disposable.Object.Dispose();
+
+            // fluent check:
+            Check.That(flag).IsTrue();
+        }
+    }
+}
---
+++ EDEngineer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EDEngineer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EDEngineer.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61829f2a-76e7-431e-a95e-484b548842b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
