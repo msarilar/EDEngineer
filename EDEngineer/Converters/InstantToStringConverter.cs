6e6f512247f29b6dc40d0cbd1c32f27dfdf2533f
git <maxime.sarilar@gmail.com>
2018-07-07 11:18:25 +0200

    better last update date

--- EDEngineer/Converters/InstantToStringConverter.cs
+++ EDEngineer/Converters/InstantToStringConverter.cs
@@ -1,14 +1,23 @@
 using System;
 using System.Globalization;
 using System.Windows.Data;
+using EDEngineer.Localization;
+using NodaTime;
 
 namespace EDEngineer.Converters
 {
     public class InstantToStringConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo _)
         {
-            return value.ToString();
+            if (value is Instant instant)
+            {
+                var culture = CultureInfo.GetCultureInfo(Languages.Instance.CurrentLanguage.TwoLetterISOLanguageName);
+                
+                return instant.ToString("ddd-MMM-yyyy HH:mm:ss", culture);
+            }
+
+            return null;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
