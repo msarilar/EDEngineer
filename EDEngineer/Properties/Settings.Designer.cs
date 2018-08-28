6612a9713a9dfe566622c0aa448bc7645934c963
git <maxime.sarilar@gmail.com>
2018-08-28 20:37:16 +0200

    prepare version 1.1.3.8

--- EDEngineer/Properties/Settings.Designer.cs
+++ EDEngineer/Properties/Settings.Designer.cs
@@ -94,7 +94,7 @@ namespace EDEngineer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.1.3.6")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.1.3.8")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
