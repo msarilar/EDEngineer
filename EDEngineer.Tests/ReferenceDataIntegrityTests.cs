8bbef065c953e8c6040268ad48b541924ddb27b0
git <maxime.sarilar@gmail.com>
2018-11-11 12:52:13 +0100

    update test for EOF line break

--- EDEngineer.Tests/ReferenceDataIntegrityTests.cs
+++ EDEngineer.Tests/ReferenceDataIntegrityTests.cs
@@ -34,7 +34,7 @@ namespace EDEngineer.Tests
                 NullValueHandling = NullValueHandling.Ignore
             });
 
-            Check.That(serialized).IsEqualTo(json);
+            Check.That(serialized + System.Environment.NewLine).IsEqualTo(json);
         }
 
         [Test]
@@ -126,7 +126,7 @@ namespace EDEngineer.Tests
                 NullValueHandling = NullValueHandling.Ignore
             });
 
-            Check.That(serialized).IsEqualTo(json);
+            Check.That(serialized + System.Environment.NewLine).IsEqualTo(json);
         }
 
         [Test]
