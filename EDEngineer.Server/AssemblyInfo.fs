cfbddf533e8db6cc4d05ff69e25ed213ca80531d
git <maxime.sarilar@gmail.com>
2020-06-20 14:10:04 +0200

    fixed manifest

--- EDEngineer.Server/AssemblyInfo.fs
+++ EDEngineer.Server/AssemblyInfo.fs
@@ -1,7 +1,6 @@
 ﻿namespace EDEngineer.Server.AssemblyInfo
 
 open System.Reflection
-open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
 // General Information about an assembly is controlled through the following 
