e8c13c7fde94a6cf2f018a8bc876f21bdfc8c59f
max <maxime.sarilar@gmail.com>
2022-08-19 16:40:16 +0200

    publish version 1.1.6.24

--- EDEngineer/Properties/Settings.Designer.cs
+++ EDEngineer/Properties/Settings.Designer.cs
@@ -94,7 +94,7 @@ namespace EDEngineer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.1.6.23")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.1.6.24")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
